66618440d0b933181a807ec6fc83528b3a9be599
SIDHARTH <maliksidharth24@gmail.com>
2026-06-05 17:42:29 +0530

    Add settings.json to configure C/C++ error squiggles; add calculating_simple_interest.exe and calulate_percentage.exe binaries; create input_areaofcircle.c to calculate the area of a circle

---
+++ input_areaofcircle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+     float r;
+     printf("what abt radius my bro \n");
+    scanf("%f",&r);
+    float pi =3.14159;
+    float a =  pi*r*r;
+    printf("Area of circle is: %f", a );
+    return 0; 
+}//sid
+
+/*
+scan ki vje s ab just input dena h at terminal answer 
+ajyga
+*/
